d7bf3b65b8f8716bdfd2533f596bb6d915e231d0
tochyodikwa <odikwatochy233@gmail.com>
2023-04-30 12:53:51 -0700

    66558666XTHG

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+if (config('app.app_sync')) {
+    Route::get('/', 'LandingController@index')->name('/');
+}
+
+if (moduleStatusCheck('Saas')) {
+    Route::group(['middleware' => ['subdomain'], 'domain' => '{subdomain}.' . config('app.short_url')], function ($routes) {
+        require('tenant.php');
+    });
+
+    Route::group(['middleware' => ['subdomain'], 'domain' => '{subdomain}'], function ($routes) {
+        require('tenant.php');
+    });
+}
+
+Route::group(['middleware' => ['subdomain']], function ($routes) {
+    require('tenant.php');
+});
+
+Route::get('migrate', function () {
+    if(Auth::check() && Auth::id() == 1){
+        \Artisan::call('migrate', ['--force' => true]);
+        \Brian2694\Toastr\Facades\Toastr::success('Migration run successfully');
+        return redirect()->to(url('/admin-dashboard'));
+    }
+   abort(404);
+});
